af474be2ee6c244ca09ea88d8c8ea5e3e37f9eec
NAVEEN MABBU <95521452+naveenmabbu@users.noreply.github.com>
2022-01-06 11:21:59 +0530

    Add files via upload

---
+++ EmployeeAttendance.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hellonaveen
+{
+    public class EmployeeAttendance
+    {
+        public static void Getattendance()
+        {
+            int IS_FULL_TIME = 1;
+            Random random = new Random();
+            int result = random.Next(0, 2);
+            if (result == IS_FULL_TIME)
+                Console.WriteLine("Employee is present");
+            else
+                Console.WriteLine("Employee is absent");
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿// See https://aka.ms/new-console-template for more information
+using hellonaveen;
+using hellonaveen.UC_10;
+using hellonaveen.UC_9;
+using hellonaveen.UC_last_class;
+
+//Console.WriteLine("Hello, World!");
+//EmployeeAttendance.Getattendance();
+//EmpWage.DailyEmployeeWage();
+//PartTimeEmpWage.EmpPartTimeWage();
+//EmpWageUsingSwitch.EmpWageBySwitch();
+//CalculateWagesFor20DaysInMonth.WageCalculatesFor20DaysInMonth();
+//CalculateWageTill100HrsOr20IsReached.WageCalculateTill100HrsOr20IsReached();
+//ComputeEmpWageUsingClassMethod.ComputeEmpWage();
+//ComputeEmpWageForMultipleCompanies.ComputeEmpWage("Dell",20,20,168);
+
+//#region file9
+//EmpWageBuilderObject reliance = new EmpWageBuilderObject("Reliance", 10, 4, 20);
+//reliance.computeEmpWage();
+//Console.WriteLine(reliance.toString());
+//reliance.computeEmpWage();
+//Console.WriteLine(reliance.toString());
+//#endregion  UC9Ending
+
+//#region file10
+//EmpWageBuilderArray empWageBuilder = new EmpWageBuilderArray();
+//empWageBuilder.addCompanyEmpWage("DMart", 20, 2, 10);
+//empWageBuilder.addCompanyEmpWage("Reliance", 10, 4, 20);
+//empWageBuilder.computeEmpWage();
+//#endregion
+
+//#region finalsolution
+//EmpWageBuilder empwagebuild = new EmpWageBuilder();
+//empwagebuild.addCompanyEmpWage("dmart", 20, 2, 10);
+//empwagebuild.addCompanyEmpWage("reliance", 10, 4, 20);
+//empwagebuild.computeEmpWage();
+//#endregion
+
+Console.ReadKey();
